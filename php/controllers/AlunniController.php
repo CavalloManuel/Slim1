471f09706a2381806396a7f3694215941a5606e3
lai2-24 <lai2-24@itismeucci.com>
2025-03-25 14:26:28 +0100

    first commit

--- php/controllers/AlunniController.php
+++ php/controllers/AlunniController.php
@@ -4,12 +4,71 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 class AlunniController
 {
-  public function index(Request $request, Response $response, $args){
+  public function index(Request $request, Response $response, $args){     //index --> visualizzazione
     $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
     $result = $mysqli_connection->query("SELECT * FROM alunni");
     $results = $result->fetch_all(MYSQLI_ASSOC);
 
     $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  public function show(Request $request, Response $response, $args){     //show --> visualizzazione con id dato
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $result = $mysqli_connection->query("SELECT * FROM alunni where id = " . $args["id"] . " "); 
+    $results = $result->fetch_all(MYSQLI_ASSOC);
+
+    $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  public function create(Request $request, Response $response, $args){     //create --> inserimento 
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+
+    $body = json_decode($request->getBody()->getContents(), true);  //prende i dati inseriti
+    $nome = $body["nome"];
+    $cognome = $body["cognome"];
+
+    $result = $mysqli_connection->query("INSERT INTO alunni (nome, cognome) VALUES ('" . $nome ."' , '" . $cognome . "') "); 
+    if($result)
+      $mess = ["msg"=>"inserimento eseguito", "ret"=>true];
+    else
+      $mess = ["msg"=>"inserimento non eseguito", "ret"=>false];
+    
+    $response->getBody()->write(json_encode($mess));
+    return $response;
+  }
+
+  public function update(Request $request, Response $response, $args){     //put --> aggiorna
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+
+    $body = json_decode($request->getBody()->getContents(), true);  //prende i dati inseriti
+    $nome = $body["nome"];
+    $cognome = $body["cognome"];
+
+    $result = $mysqli_connection->query("UPDATE alunni SET nome = '$nome', cognome = '$cognome' WHERE id = " . $args["id"] . " "); 
+    
+    if($result)
+      $mess = ["msg"=>"inserimento eseguito", "ret"=>true];
+    else
+      $mess = ["msg"=>"inserimento non eseguito", "ret"=>false];
+    
+    $response->getBody()->write(json_encode($mess));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  public function destroy(Request $request, Response $response, $args){     //delete --> elimina
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+
+
+
+    $result = $mysqli_connection->query("DELETE FROM alunni WHERE id = " . $args["id"] . " "); 
+    if($result)
+      $mess = ["msg"=>"inserimento eseguito", "ret"=>true];
+    else
+      $mess = ["msg"=>"inserimento non eseguito", "ret"=>false];
+    
+    $response->getBody()->write(json_encode($mess));
     return $response->withHeader("Content-type", "application/json")->withStatus(200);
   }
 }
--- php/index.php
+++ php/index.php
@@ -6,6 +6,23 @@ require __DIR__ . '/controllers/AlunniController.php';
 
 $app = AppFactory::create();
 
+// curl http://localhost:8080/alunni
 $app->get('/alunni', "AlunniController:index");
+
+// curl http://localhost:8080/alunni/2
+$app->get('/alunni/{id:\d+}', "AlunniController:show");  //mostro alunno con id specifico
+
+// curl -X POST http://localhost:8080/alunni -H "Content-Type: application/json" -d '{"nome": "ciccio", "cognome": "bello"}'
+$app->post('/alunni', "AlunniController:create");  //aggiungo alunno
+
+// curl -X PUT http://localhost:8080/alunni/2 -H "Content-Type: application/json" -d '{"nome": "ciccio", "cognome": "bello"}'
+$app->put('/alunni/{id:\d+}', "AlunniController:update");  //aggiorno l'alunno
+
+// curl -X DELETE http://localhost:8080/alunni/2
+$app->delete('/alunni/{id:\d+}', "AlunniController:destroy");  //elimina alunno
+
+
+
+
 
 $app->run();
